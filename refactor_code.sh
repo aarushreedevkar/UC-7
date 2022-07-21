679302f6aa40e54df64cfe52fea38bb0d6f254de
jyoti <jyotidhage10@gmail.com>
2022-07-21 11:00:32 +0530

    jyoti]created UC-7

---
+++ refactor_code.sh
@@ -0,0 +1,27 @@
+#! /bin/bash -x
+
+STARTTIME=`date +%s`
+echo "`date`: Starting the script."
+hms()
+{
+ local SECONDS H M S MM H_TAG M_TAG S_TAG
+  SECONDS=${1:-0}
+  let S=${SECONDS}%60
+  let MM=${SECONDS}/60 # Total number of minutes
+  let M=${MM}%60
+  let H=${MM}/60
+  
+  if [ "$2" == "long" ]; then
+    [ "$H" -eq "1" ] && H_TAG="hour" || H_TAG="hours"
+    [ "$M" -eq "1" ] && M_TAG="minute" || M_TAG="minutes"
+    [ "$S" -eq "1" ] && S_TAG="second" || S_TAG="seconds"
+    [ "$H" -gt "0" ] && printf "%d %s " $H "${H_TAG},"
+    [ "$SECONDS" -ge "60" ] && printf "%d %s " $M "${M_TAG} and"
+    printf "%d %s\n" $S "${S_TAG}"
+  else
+    
+[ "$H" -gt "0" ] && printf "%02d%s" $H "h"
+    [ "$M" -gt "0" ] && printf "%02d%s" $M "m"
+    printf "%02d%s\n" $S "s"
+  fi
+}
